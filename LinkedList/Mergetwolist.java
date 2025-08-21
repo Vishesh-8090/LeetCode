dbe098a9c8b38a97b08d2bff68224a5cef3b305d
Vishesh-8090 <visheshraj385@gmail.com>
2025-08-21 21:52:44 +0530

    first commit

---
+++ LinkedList/Mergetwolist.java
@@ -0,0 +1,41 @@
+package LinkedList;
+public class Mergetwolist {
+        public static void main(String[] args) {
+            ListNode l1 = new ListNode();
+            l1.insertf(3);
+            l1.insertf(-9);
+
+            ListNode l2 = new ListNode();
+            l2.insertf(7);
+            l2.insertf(5);
+
+            l1.display();
+            l2.display();
+
+            ListNode res = new ListNode();
+            System.out.println(mergeTwoLists(l1.head, l2.head, res));
+            res.display();
+        }
+
+        public static ListNode mergeTwoLists(ListNode list1, ListNode list2, ListNode res) {
+            ListNode k = res;
+
+            while (list1 != null && list2 != null) {
+                if (list1.val <= list2.val) {
+                    k.next = list1;
+                    list1 = list1.next;
+                } else {
+                    k.next = list2;
+                    list2 = list2.next;
+                }
+                k = k.next;
+            }
+            if (list1 != null) {
+                k.next = list1;
+            }
+            if (list2 != null) {
+                k.next = list2;
+            }
+            return res.next;
+        }
+}
