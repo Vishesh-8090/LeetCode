2a405c8d0b3114c192eb84bba99e95e624b04b39
Vishesh-8090 <visheshraj385@gmail.com>
2025-09-22 00:21:43 +0530

    875. Koko Eating Bananas

---
+++ linear_Binary/KoKo.java
@@ -0,0 +1,38 @@
+package linear_Binary;
+
+public class KoKo {
+    public static void main(String[] args) {
+        int[] arr= {3,6,7,11};
+        System.out.println(minEatingSpeed(arr, 8));
+    }
+    public static int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = max_hr(piles);
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(totaltime(piles, h, mid)){
+                right = mid;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    public static int max_hr(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            if(max < arr[i]){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public static boolean totaltime(int[] arr, int h, int k){
+        long total_hr = 0;
+        for(int i=0; i<arr.length; i++){
+            total_hr += (arr[i] + k - 1)/k;
+        }
+        return total_hr <= h;
+    }
+}
