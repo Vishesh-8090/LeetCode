d7764b726118cebf12c5526b71fda013148106b4
Vishesh-8090 <visheshraj385@gmail.com>
2026-03-15 16:58:49 +0530

    More Optimized

--- Arrays/remove_duplicates.java
+++ Arrays/remove_duplicates.java
@@ -6,16 +6,16 @@ public class remove_duplicates {
     public static void main(String[] args) {
         int[] arr = {0,0,1,1,1,1,2,3,3};
         System.out.println(removeDuplicates(arr));
-        System.out.println(Arrays.toString(arr));
     }
     public static int removeDuplicates(int[] nums) {
-        int i = 0;
-        for(int j=2; j<nums.length; j++){
+        int i=0;
+        int c = 1;
+        for(int j=1; j<nums.length; j++){
             if(nums[i] != nums[j]){
-                nums[i+2] = nums[j];
-                i++;
+                nums[++i] = nums[j];
+                c++;
             }
         }
-        return i+2;
+        return c;
     }
 }
