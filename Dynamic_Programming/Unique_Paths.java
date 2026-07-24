9a884fbadae7c6167f60751b368a89e5a3e71526
Vishesh-8090 <visheshraj385@gmail.com>
2026-07-24 21:11:06 +0530

    62. Unique Paths

---
+++ Dynamic_Programming/Unique_Paths.java
@@ -0,0 +1,27 @@
+package Dynamic_Programming;
+
+import java.util.Arrays;
+
+public class Unique_Paths {
+    public static void main(String[] args) {
+        int m = 19;
+        int n = 13;
+
+        int[][] dp = new int[m][n];
+        for(int i=0; i<m; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        System.out.println(helper(m-1, n-1, dp));
+    }
+    private static int helper(int i, int j, int[][] dp){
+        if(i == 0 && j == 0) return 1;
+        if(i < 0 || j < 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = helper(i-1, j, dp);
+        int left = helper(i, j-1, dp);
+
+        return dp[i][j] = up + left;
+    }
+}
